14d13a1d0bd1a175240177384a8f86343a843a6e
Antonio Takev <tonitakev@gmail.com>
2019-06-07 00:03:33 +0300

    adding two tables in the model

--- django project/web/crud/models.py
+++ django project/web/crud/models.py
@@ -2,9 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name + " " + self.category
+
 class Member(models.Model):
     firstname = models.CharField(max_length=40)
     lastname = models.CharField(max_length=40)
 
     def __str__(self):
         return self.firstname + " " + self.lastname
+
+class BookMember(models.Model):
+    book = models.ForeignKey(Book,default=None)
+    member = models.ForeignKey(Member,default=None)
